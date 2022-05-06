60111c2969c443571b8ac3e021d314a245a92c8a
flavicrisan92 <flavicrisan92@gmail.com>
2022-05-06 11:08:37 +0300

    Command controller and actions

--- CommandsService/Controllers/PlatformsController.cs
+++ CommandsService/Controllers/PlatformsController.cs
@@ -1,3 +1,6 @@
+using AutoMapper;
+using CommandsService.Data;
+using CommandsService.Dtos;
 using Microsoft.AspNetCore.Mvc;
 
 namespace CommandsService.Contollers;
@@ -6,9 +9,13 @@ namespace CommandsService.Contollers;
 [ApiController]
 public class PlatformsController : ControllerBase
 {
-    public PlatformsController()
-    {
+    private readonly ICommandRepo _repository;
+    private readonly IMapper _mapper;
 
+    public PlatformsController(ICommandRepo repository, IMapper mapper)
+    {
+        _repository = repository;
+        _mapper = mapper;
     }
 
     [HttpPost]
@@ -16,5 +23,14 @@ public class PlatformsController : ControllerBase
     {
         Console.WriteLine("--> Inbound POST # Command Service");
         return Ok("Inbound test of from Platforms Controller");
+    }
+
+    [HttpGet]
+    public ActionResult<IEnumerable<PlatformReadDto>> GetPlatforms()
+    {
+        Console.WriteLine("--> Get Platforms from CommandService");
+        var platformItesms = _repository.GetAllPlatforms();
+
+        return (Ok(_mapper.Map<IEnumerable<PlatformReadDto>>(platformItesms)));
     }
 }
